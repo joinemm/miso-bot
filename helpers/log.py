56614b67c91eccd501e520993d7e0c7641c3f331
joinemm <joonas.rautiola@hotmail.com>
2019-03-17 18:24:27 +0200

    better log formatting

--- helpers/log.py
+++ helpers/log.py
@@ -2,35 +2,35 @@ import logging
 import sys
 
 FORMATTER = logging.Formatter("[ %(asctime)s | %(levelname)s | %(name)s.%(funcName)s() ]:: %(message)s",
-                              datefmt='%y/%m/%d %H:%M:%S')
+                              datefmt='%d/%m/%y %H:%M:%S')
 FORMATTER_COMMANDS = logging.Formatter("[ %(asctime)s | COMMAND | %(message)s",
-                                       datefmt='%y/%m/%d %H:%M:%S')
+                                       datefmt='%d/%m/%y %H:%M:%S')
 
 
 def get_logger(logger_name):
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)  # better to have too much log than not enough
+    if logger.handlers:
+        return logger
 
+    # logger not created yet, assign options
+    logger.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(FORMATTER)
     logger.addHandler(console_handler)
-
-    # with this pattern, it's rarely necessary to propagate the error up to parent
-    logger.propagate = False
 
     return logger
 
 
 def get_command_logger():
     logger = logging.getLogger("commands")
-    logger.setLevel(logging.DEBUG)  # better to have too much log than not enough
+    if logger.handlers:
+        return logger
 
+    # logger not created yet, assign options
+    logger.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(FORMATTER_COMMANDS)
     logger.addHandler(console_handler)
-
-    # with this pattern, it's rarely necessary to propagate the error up to parent
-    logger.propagate = False
 
     return logger
 
