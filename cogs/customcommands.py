5f234b4784beee6e22478cf8e4f88ada0979c3dc
Joinemm <joonas@rautiola.co>
2019-03-21 17:56:19 +0200

    converted custom commands to use commandgroups

--- cogs/customcommands.py
+++ cogs/customcommands.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 import data.database as db
 import helpers.errormessages as errormsg
+import helpers.utilityfunctions as util
+import helpers.log as log
+
+command_logger = log.get_command_logger()
 
 
 class CustomCommands(commands.Cog):
@@ -14,14 +18,15 @@ class CustomCommands(commands.Cog):
         """ only for CommandNotFound"""
         error = getattr(error, 'original', error)
         if isinstance(error, commands.CommandNotFound):
-            keyword = ctx.message.content.strip(">").split(" ")[0]
+            keyword = ctx.message.content.split(' ', 1)[0][len(self.client.command_prefix):]
             data = db.query("SELECT response FROM customcommands WHERE guild_id = ? and command = ?",
                             (ctx.guild.id, keyword), maketuple=True)
             if data is not None:
+                command_logger.info(log.custom_command_format(ctx, keyword))
                 return await ctx.send(data.response)
 
     def client_command_list(self, match=""):
-        """Get list of client commands"""
+        """:returns list of client commands"""
         command_list = set()
         for command in self.client.commands:
             if match in command.name:
@@ -31,85 +36,71 @@ class CustomCommands(commands.Cog):
                     command_list.add(alias)
         return command_list
 
-    @commands.command()
-    async def command(self, ctx, method, name=None, *args):
+    @commands.group()
+    async def command(self, ctx):
         """Configuration for server specific custom commmands"""
+        await util.command_group_help(ctx)
 
-        # check if name parameter is specified for the methods that need them
-        if method in ['add', 'remove', 'search']:
-            if name in [None, ""] or name.isspace():
-                return await ctx.send(errormsg.missing_parameter('name'))
+    @command.command()
+    async def add(self, ctx, name, *, response):
+        """<keyword> <response...>"""
+        if name in self.client_command_list():
+            return await ctx.send(f"Sorry, `{self.client.command_prefix}{name}` is already a built in command!")
+        elif name in custom_command_list(ctx.guild.id):
+            return await ctx.send(f"Sorry, the custom command `{self.client.command_prefix}{name}` "
+                                  f"already exists on this server!")
+        if len(response) < 1:
+            return await ctx.send(errormsg.missing_parameter('response'))
 
-        # add a new command to the server
-        if method == "add":
-            if name in self.client_command_list():
-                return await ctx.send(f"Sorry, `{self.client.command_prefix}{name}` is already a built in command!")
-            elif name in custom_command_list(ctx.guild.id):
-                return await ctx.send(f"Sorry, the custom command `{self.client.command_prefix}{name}` already exists on this server!")
+        db.execute("REPLACE INTO customcommands VALUES (?, ?, ?)", (ctx.guild.id, name, response))
+        await ctx.send(f"Custom command `{self.client.command_prefix}{name}` "
+                       f"successfully added with the response `{response}`")
 
-            response = " ".join(args)
-            if len(response) < 1:
-                return await ctx.send(errormsg.missing_parameter('response'))
+    @command.command()
+    async def remove(self, ctx, name):
+        if name not in custom_command_list(ctx.guild.id):
+            return await ctx.send(f"Cannot delete command `{self.client.command_prefix}{name}` as it does not exist")
 
-            db.execute("REPLACE INTO customcommands VALUES (?, ?, ?)", (ctx.guild.id, name, response))
-            await ctx.send(f"Custom command `{self.client.command_prefix}{name}` successfully added with the response `{response}`")
+        db.execute("DELETE FROM customcommands WHERE guild_id = ? and command = ?", (ctx.guild.id, name))
+        await ctx.send(f"Custom command `{self.client.command_prefix}{name}` successfully deleted")
 
-        # remove a command from the server
-        elif method == "remove":
-            if name not in custom_command_list(ctx.guild.id):
-                return await ctx.send(f"Cannot delete command `{self.client.command_prefix}{name}` as it does not exist")
+    @command.command()
+    async def search(self, ctx, name):
+        content = discord.Embed()
 
-            db.execute("DELETE FROM customcommands WHERE guild_id = ? and command = ?", (ctx.guild.id, name))
-            await ctx.send(f"Custom command `{self.client.command_prefix}{name}` successfully deleted")
+        internal_rows = []
+        for command in self.client_command_list(match=name):
+            internal_rows.append(f"{self.client.command_prefix}{command}")
+        if internal_rows:
+            content.add_field(name="Internal commands", value="\n".join(internal_rows))
 
-        # search for a command
-        elif method == "search":
-            content = discord.Embed()
+        custom_rows = []
+        for command in custom_command_list(ctx.guild.id, match=name):
+            custom_rows.append(f"{self.client.command_prefix}{command}")
+        if custom_rows:
+            content.add_field(name="Custom commands", value="\n".join(custom_rows))
 
-            internal_rows = []
-            for command in self.client_command_list(match=name):
-                internal_rows.append(f"{self.client.command_prefix}{command}")
-            if internal_rows:
-                content.add_field(name="Internal commands", value="\n".join(internal_rows))
-
-            custom_rows = []
-            for command in custom_command_list(ctx.guild.id, match=name):
-                custom_rows.append(f"{self.client.command_prefix}{command}")
-            if custom_rows:
-                content.add_field(name="Custom commands", value="\n".join(custom_rows))
-
-            if content.fields:
-                await ctx.send(embed=content)
-            else:
-                await ctx.send("**Found nothing!**")
-
-        # get a list of all custom commands on this server
-        elif method == "list":
-            rows = []
-            for command in custom_command_list(ctx.guild.id):
-                rows.append(f"{self.client.command_prefix}{command}")
-
-            if rows:
-                content = discord.Embed(title=f"{ctx.guild.name} commands")
-                content.description = "\n".join(rows)
-                await ctx.send(embed=content)
-            else:
-                await ctx.send("No custom commands added on this server yet")
-
-        # get help
-        elif method == "help":
-            await ctx.send("`>command add [name] [response]`\n"
-                           "`>command remove [name]`\n"
-                           "`>command search [name]`\n"
-                           "`>command list`\n"
-                           "`>command help`")
-
+        if content.fields:
+            await ctx.send(embed=content)
         else:
-            await ctx.send(errormsg.invalid_method(ctx.command, method))
+            await ctx.send("**Found nothing!**")
+
+    @command.command()
+    async def list(self, ctx):
+        rows = []
+        for command in custom_command_list(ctx.guild.id):
+            rows.append(f"{self.client.command_prefix}{command}")
+
+        if rows:
+            content = discord.Embed(title=f"{ctx.guild.name} commands")
+            content.description = "\n".join(rows)
+            await ctx.send(embed=content)
+        else:
+            await ctx.send("No custom commands added on this server yet")
 
 
 def custom_command_list(guild_id, match=""):
-    """Get list of custom commands on server"""
+    """:returns list of custom commands on server"""
     command_list = set()
     data = db.query("SELECT command FROM customcommands WHERE guild_id = ?", (guild_id,))
     if data is not None and len(data) > 0:
