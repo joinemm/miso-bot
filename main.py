2611452e31155cd56bbc63de46eb0e913c9e382e
joinemm <joonas.rautiola@hotmail.com>
2019-03-21 17:59:12 +0200

    import new cogs, commands now case insensitive

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ logger = log.get_logger(__name__)
 command_logger = log.get_command_logger()
 
 TOKEN = os.environ.get('MISO_BOT_TOKEN_BETA')
-client = commands.Bot(command_prefix="<")
+client = commands.Bot(command_prefix=">", case_insensitive=True)
 
-extensions = ['cogs.events', 'cogs.errorhandler', 'cogs.customcommands', 'cogs.fishy']
+extensions = ['cogs.events', 'cogs.config', 'cogs.errorhandler', 'cogs.customcommands', 'cogs.fishy',
+              'cogs.rolepicker', 'cogs.mod', 'cogs.owner']
 
 
 @client.event
