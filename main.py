4fcbfdc3106a1e96ae9b95abf6e845a3b27f648b
Joinemm <joonas@rautiola.co>
2019-06-06 17:18:31 +0300

    prefix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ logger = log.get_logger(__name__)
 command_logger = log.get_command_logger()
 
 TOKEN = os.environ.get('MISO_BOT_TOKEN')
-client = commands.Bot(command_prefix='<', case_insensitive=True)
+client = commands.Bot(command_prefix='>', case_insensitive=True)
 
 extensions = ['cogs.events', 'cogs.config', 'cogs.errorhandler', 'cogs.customcommands', 'cogs.fishy', 'cogs.info',
               'cogs.rolepicker', 'cogs.mod', 'cogs.owner', 'cogs.notifications', 'cogs.miscellaneous', 'cogs.media',
