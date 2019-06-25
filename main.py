c732953ec01165507cc3a5cb1ddeb212cd1be989
Joinemm <joonas@rautiola.co>
2019-06-25 21:19:02 +0300

    typing on every command

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ client = commands.Bot(command_prefix='>', case_insensitive=True)
 
 extensions = ['cogs.events', 'cogs.config', 'cogs.errorhandler', 'cogs.customcommands', 'cogs.fishy', 'cogs.info',
               'cogs.rolepicker', 'cogs.mod', 'cogs.owner', 'cogs.notifications', 'cogs.miscellaneous', 'cogs.media',
-              'cogs.lastfm', 'cogs.user', 'cogs.images'
+              'cogs.lastfm', 'cogs.user', 'cogs.images', 'cogs.utility'
               ]
 
 
@@ -21,6 +21,7 @@ async def on_ready():
 
 @client.before_invoke
 async def before_any_command(ctx):
+    await ctx.trigger_typing()
     if ctx.invoked_subcommand is None:
         command_logger.info(log.log_command(ctx))
 
