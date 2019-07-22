ec7f5c0477249b20d1d5ad560176bf6d748e11e5
Joinemm <joonas.rautiola@hotmail.com>
2019-07-22 16:27:40 +0300

    sql pretty printer

--- data/database.py
+++ data/database.py
@@ -143,3 +143,32 @@ def getter(d, key):
             return d[int(key)]
         except (ValueError, IndexError):
             return None
+
+
+def pp(cursor, data=None, rowlens=0):
+    d = cursor.description
+    if not d:
+        return "#### NO RESULTS ###"
+    names = []
+    lengths = []
+    rules = []
+    if not data:
+        data = cursor.fetchall(  )
+    for dd in d:    # iterate over description
+        l = dd[1]
+        if not l:
+            l = 12             # or default arg ...
+        l = max(l, len(dd[0])) # Handle long names
+        names.append(dd[0])
+        lengths.append(l)
+    for col in range(len(lengths)):
+        if rowlens:
+            rls = [len(row[col]) for row in data if row[col]]
+            lengths[col] = max([lengths[col]]+rls)
+        rules.append("-"*lengths[col])
+    format = " ".join(["%%-%ss" % l for l in lengths])
+    result = [format % tuple(names)]
+    result.append(format % tuple(rules))
+    for row in data:
+        result.append(format % row)
+    return "\n".join(result)
