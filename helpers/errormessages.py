8f3f3b35a6932899487d8c3e2f417b7f29d23e5b
joinemm <joonas.rautiola@hotmail.com>
2019-08-09 21:58:57 +0300

    refactored some configuration commands

--- helpers/errormessages.py
+++ helpers/errormessages.py
@@ -2,27 +2,27 @@ PREFIX = ">"
 
 
 def invalid_method(command, method):
-    return f"**ERROR:** Invalid method `{method}`. Use `{PREFIX}{command} help` to get help"
+    return f"Invalid subcommand `{method}`. Use `{PREFIX}{command} help` to get help"
 
 
 def invalid_argument(command, method, argument):
-    return f"**ERROR:** Invalid argument `{argument}` for {method}. Use `{PREFIX}{command} help` to get help"
+    return f"Invalid argument `{argument}` for {method}. Use `{PREFIX}{command} help` to get help"
 
 
 def channel_not_found(channel):
-    return f"**ERROR:** Channel {channel} not found"
+    return f"Channel {channel} not found"
 
 
 def user_not_found(user):
-    return f"**ERROR:** User {user} not found"
+    return f"User {user} not found."
 
 
 def role_not_found(role):
-    return f"**ERROR:** Role {role} not found"
+    return f"Role {role} not found."
 
 
 def missing_parameter(name):
-    return f"**ERROR:** Missing parameter `{name}`"
+    return f"Required argument `{name}` is missing."
 
 
 
