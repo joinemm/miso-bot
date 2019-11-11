6d5d18723b22c2c748d7b5677336f3da75667d6c
Joinemm <joonas@rautiola.co>
2019-11-11 19:42:09 +0200

    new colored logs

--- helpers/log.py
+++ helpers/log.py
@@ -1,14 +1,7 @@
 import logging
 import sys
-
-FORMATTER = logging.Formatter(fmt="{asctime} | {levelname:7} | {module:>17}.{funcName:17} | {message}",
-                              datefmt='%d/%m/%y %H:%M:%S', style='{')
-
-FORMATTER_COMMANDS = logging.Formatter(fmt="{asctime} | COMMAND | {message}",
-                                       datefmt='%d/%m/%y %H:%M:%S', style='{')
-
-FORMATTER_COMMANDS_LEVELS = logging.Formatter(fmt="{asctime} | {levelname:7} | {message}",
-                                              datefmt='%d/%m/%y %H:%M:%S', style='{')
+import coloredlogs
+from time import time
 
 
 def get_logger(logger_name):
@@ -17,39 +10,41 @@ def get_logger(logger_name):
         return logger
 
     # logger not created yet, assign options
-    logger.setLevel(logging.DEBUG)
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(FORMATTER)
-    logger.addHandler(console_handler)
+    coloredlogs.install(
+        fmt="{asctime} | {levelname:7} | {name:25} > {message}",
+        style='{',
+        level='DEBUG',
+        logger=logger
+    )
 
     return logger
 
 
-def get_command_logger(showlevel=False):
-    logger = logging.getLogger("commands" + ("withlevels" if showlevel else ""))
+def get_command_logger():
+    logger = logging.getLogger("commands")
     if logger.handlers:
         return logger
 
     # logger not created yet, assign options
-    logger.setLevel(logging.DEBUG)
-    console_handler = logging.StreamHandler(sys.stdout)
-    if showlevel:
-        console_handler.setFormatter(FORMATTER_COMMANDS_LEVELS)
-    else:
-        console_handler.setFormatter(FORMATTER_COMMANDS)
-    logger.addHandler(console_handler)
+    coloredlogs.install(
+        fmt="{asctime} | {levelname:7} | {message}",
+        style='{',
+        level='DEBUG',
+        logger=logger
+    )
 
     return logger
 
 
 def log_command(ctx):
-    command = str(ctx.command)+'()'
+    took = time() - ctx.timer
+    command = str(ctx.command)
     guild = ctx.guild.name if ctx.guild is not None else 'DM'
     user = str(ctx.author)
-    return f"{command:>16} > {guild:16} | {user:20} \"{ctx.message.content}\""
+    return f"{command:19} {took:.2f}s > {guild} ? {user} \"{ctx.message.content}\""
 
 
 def custom_command_format(ctx, keyword):
     guild = ctx.guild.name if ctx.guild is not None else 'DM'
     user = str(ctx.author)
-    return f"{f'custom({keyword})':>16} > {guild:16} | {user:20} \"{ctx.message.content}\""
+    return f"{f'custom({keyword})':>24} > {guild} ? {user} \"{ctx.message.content}\""
