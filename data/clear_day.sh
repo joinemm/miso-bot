ff361834af92f0272fe364cb18ef55010021ff59
Joinemm <joonas@rautiola.co>
2020-05-24 13:51:48 +0000

    fix database clearing scripts

--- data/clear_day.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sqlite3 /home/join/miso-bot/data/database.db <<EOF
-DELETE FROM activity_day;
-VACUUM;
-EOF
--- data/clear_month.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sqlite3 /home/join/miso-bot/data/database.db <<EOF
-DELETE FROM activity_month;
-VACUUM;
-EOF
