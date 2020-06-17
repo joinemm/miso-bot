54e75a816db5c21cb075af0f9b6158ad12bed232
Joinemm <joonas.rautiola@hotmail.com>
2020-06-17 12:24:05 +0300

    added fm servernp command

--- cogs/lastfm.py
+++ cogs/lastfm.py
@@ -895,6 +895,56 @@ class LastFm(commands.Cog):
                             break
                         f.write(block)
 
+    @fm.command(aliases=["snp"])
+    async def servernp(self, ctx):
+        """What people on this server are listening to at the moment."""
+        listeners = []
+        tasks = []
+        userslist = db.query(
+            "SELECT user_id, lastfm_username FROM users where lastfm_username is not null"
+        )
+        for user in userslist if userslist is not None else []:
+            lastfm_username = user[1]
+            member = ctx.guild.get_member(user[0])
+            if member is None:
+                continue
+
+            tasks.append(get_np(lastfm_username, member))
+
+        total_linked = len(tasks)
+        if tasks:
+            data = await asyncio.gather(*tasks)
+            for song, member_ref in data:
+                if song is not None:
+                    listeners.append((song, member_ref))
+        else:
+            return await ctx.send(
+                "Nobody on this server has connected their last.fm account yet!"
+            )
+
+        if not listeners:
+            return await ctx.send(
+                "Nobody on this server is listening to anything at the moment!"
+            )
+
+        total_listening = len(listeners)
+        rows = []
+        for song, member in listeners:
+            rows.append(
+                f"{member.mention} **{song.get('artist')}** — ***{song.get('name')}***"
+            )
+
+        content = discord.Embed()
+        content.set_author(
+            name=f"What is {ctx.guild.name} listening to?",
+            icon_url=ctx.guild.icon_url_as(size=64),
+        )
+        content.colour = int(
+            await util.color_from_image_url(str(ctx.guild.icon_url_as(size=64))), 16
+        )
+        content.set_footer(text=f"{total_listening} / {total_linked} Members")
+        await util.send_as_pages(ctx, content, rows)
+
     @commands.command(aliases=["wk"])
     @commands.guild_only()
     @commands.cooldown(2, 15, type=commands.BucketType.user)
@@ -1228,6 +1278,25 @@ async def get_playcount(artist, username, reference=None):
         return count
     else:
         return count, reference, name
+
+
+async def get_np(username, ref):
+    data = await api_request(
+        {"method": "user.getrecenttracks", "user": username, "limit": 1},
+        ignore_errors=True,
+    )
+    song = None
+    if data is not None:
+        tracks = data["recenttracks"]["track"]
+        if tracks:
+            if "@attr" in tracks[0]:
+                if "nowplaying" in tracks[0]["@attr"]:
+                    song = {
+                        "artist": tracks[0]["artist"]["#text"],
+                        "name": tracks[0]["name"],
+                    }
+
+    return song, ref
 
 
 async def get_similar_artists(artistname):
