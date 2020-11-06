3bd7e4e15aed1dbf6023e74fd454ecb5db7b7887
Joinemm <joonas@rautiola.co>
2020-11-06 20:53:14 +0200

    fix reddit embed when redditor has no icon image

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -163,7 +163,9 @@ class Reddit(commands.Cog):
             content.set_author(
                 name=f"u/{redditor.name}",
                 url=f"https://old.reddit.com/u/{redditor.name}",
-                icon_url=(redditor.icon_img if hasattr(redditor, "icon_img") else None),
+                icon_url=(
+                    redditor.icon_img if hasattr(redditor, "icon_img") else discord.Embed.Empty
+                ),
             )
 
         suffix_elements = [
