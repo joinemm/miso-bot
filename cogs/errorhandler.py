0861ee20f908001a2530daad53779f2a2a97d91a
Joinemm <joonas@rautiola.co>
2020-11-26 15:19:00 +0200

    new errorhandler using fancy embeds

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -9,20 +9,67 @@ logger = log.get_logger(__name__)
 command_logger = log.get_logger("commands")
 
 
-class Events(commands.Cog):
+class ErrorHander(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.message_levels = {
+            "info": {
+                "description_prefix": ":information_source:",
+                "color": int("3b88c3", 16),
+                "help_footer": False,
+            },
+            "warning": {
+                "description_prefix": ":warning:",
+                "color": int("ffcc4d", 16),
+                "help_footer": False,
+            },
+            "error": {
+                "description_prefix": ":no_entry:",
+                "color": int("be1931", 16),
+                "help_footer": False,
+            },
+            "cooldown": {
+                "description_prefix": ":hourglass_flowing_sand:",
+                "color": int("ffe8b6", 16),
+                "help_footer": False,
+            },
+        }
+
+    async def send(self, ctx, level, message, help_footer=None):
+        """Send error message to chat."""
+        settings = self.message_levels.get(level)
+        embed = discord.Embed(
+            color=settings["color"], description=f"{settings['description_prefix']} `{message}`"
+        )
+
+        help_footer = help_footer or settings["help_footer"]
+        if help_footer:
+            embed.set_footer(text=f"Learn more: {ctx.prefix}help {ctx.command.qualified_name}")
+
+        try:
+            await ctx.send(embed=embed)
+        except discord.errors.Forbidden:
+            self.bot.logger.warning("Forbidden when trying to send error message embed")
+
+    async def log_and_traceback(self, ctx, error):
+        logger.error(f'Unhandled exception in command "{ctx.message.content}":')
+        exc = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+        logger.error(exc)
+        await self.send(ctx, "error", f"{type(error).__name__}: {error}")
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         """The event triggered when an error is raised while invoking a command."""
 
+        # ignore if command has it's own error handler
         if hasattr(ctx.command, "on_error"):
             return
 
+        # extract the original error from the CommandError wrapper
         error = getattr(error, "original", error)
 
-        if isinstance(error, commands.CommandNotFound):
+        # silently ignored expections
+        if isinstance(error, (commands.CommandNotFound, commands.DisabledCommand)):
             return
 
         if isinstance(error, commands.MissingRequiredArgument):
@@ -32,75 +79,83 @@ class Events(commands.Cog):
             f'{type(error).__name__:25} > {ctx.guild} ? {ctx.author} "{ctx.message.content}" > {error}'
         )
 
-        if isinstance(error, util.ErrorMessage):
-            return await ctx.send(str(error))
+        if isinstance(error, exceptions.Info):
+            await self.send(ctx, "info", str(error))
 
-        if isinstance(error, commands.MissingPermissions):
+        elif isinstance(error, exceptions.Warning):
+            await self.send(ctx, "warning", str(error))
+
+        elif isinstance(error, exceptions.Blacklist):
+            await self.send(ctx, "error", error.message)
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            try:
+                await self.send(
+                    ctx.author,
+                    "info",
+                    "This command cannot be used in private messages",
+                )
+            except (discord.HTTPException, discord.errors.Forbidden):
+                pass
+
+        elif isinstance(error, commands.MissingPermissions):
             perms = ", ".join(f"`{x}`" for x in error.missing_perms)
-            return await ctx.send(f":warning: You require {perms} permission to use this command!")
+            await self.send(ctx, "warning", f"You require {perms} permission to use this command!")
 
         elif isinstance(error, commands.BotMissingPermissions):
             perms = ", ".join(f"`{x}`" for x in error.missing_perms)
-            return await ctx.send(
-                f":warning: Cannot execute command! Bot is missing permission {perms}"
+            await self.send(
+                ctx, "warning", f"Cannot execute command! Bot is missing permission {perms}"
             )
 
         elif isinstance(error, commands.CommandOnCooldown):
-            if db.is_patron(ctx.author.id, (2, 3)):
+            if db.is_patron(ctx.author.id, (2, 3, 4)):
                 return await ctx.reinvoke()
             else:
-                return await ctx.send(
-                    f":hourglass_flowing_sand: You are on cooldown. Please wait `{error.retry_after:.0f} seconds`"
+                await self.send(
+                    ctx,
+                    "cooldown",
+                    f"You are on cooldown. Please wait `{error.retry_after:.0f} seconds`",
                 )
 
         elif isinstance(error, commands.errors.MaxConcurrencyReached):
             await ctx.send("Stop spamming! >:(")
 
-        elif isinstance(error, commands.DisabledCommand):
-            await ctx.send(f":warning: `{ctx.command}` has been disabled!")
-
         elif isinstance(error, commands.NoPrivateMessage):
-            await ctx.author.send(":warning: You cannot use this command in private messages")
+            await self.send(ctx, "info", "You cannot use this command in private messages")
 
         elif isinstance(error, util.PatronCheckFailure):
-            await ctx.send(
-                ":no_entry: Support me on patreon to use this command! <https://patreon.com/joinemm>"
+            await self.send(
+                ctx,
+                "error",
+                "Support me on patreon to use this command! <https://patreon.com/joinemm>",
             )
 
         elif isinstance(error, (commands.NotOwner, commands.CheckFailure)):
-            await ctx.send(":warning: Sorry, you are not authorized to use this command!")
+            await self.send(ctx, "error", "Sorry, you are not authorized to use this command!")
 
-        elif isinstance(error, exceptions.BlacklistTrigger):
-            if error.blacklist_type == "command":
-                message = "This command has been blacklisted by the server moderators"
-            elif error.blacklist_type == "channel":
-                message = (
-                    "Command usage on this channel has been blacklisted by the server moderators"
-                )
-            elif error.blacklist_type == "user":
-                message = "You have been blacklisted from using commands by the server moderators"
-            elif error.blacklist_type == "global":
-                message = "You have been blacklisted from using Miso Bot"
-            elif error.blacklist_type == "guild":
-                message = "This server is blacklisted from using Miso Bot"
-
-            delete = error.do_delete
-            await ctx.send(f":no_entry_sign: `{message}`", delete_after=(5 if delete else None))
+        elif isinstance(error, exceptions.Blacklist):
+            delete = await self.bot.db.execute(
+                "SELECT delete_blacklisted_usage FROM guild_settings WHERE guild_id = %s",
+                ctx.guilds.id,
+                one_value=True,
+            )
+            await self.send(ctx, "error", error.message, delete_after=(5 if delete else None))
             if delete:
                 await asyncio.sleep(5)
                 await ctx.message.delete()
 
         elif isinstance(error, (commands.BadArgument, flags._parser.ArgumentParsingError)):
-            await ctx.send(f"```{str(error)}```")
+            await self.send(ctx, "warning", str(error), help_footer=True)
 
         elif isinstance(error, discord.errors.Forbidden):
             try:
-                await ctx.send(f"```{str(error)}```")
+                await self.send(ctx, "error", str(error))
             except discord.errors.Forbidden:
                 try:
                     await ctx.message.add_reaction("🙊")
                 except discord.errors.Forbidden:
-                    logger.error(str(error))
+                    await self.log_and_traceback(ctx, error)
 
         elif isinstance(error, exceptions.LastFMError):
             if error.error_code == 8:
@@ -112,12 +167,11 @@ class Events(commands.Cog):
             else:
                 message = error.display()
 
-            await ctx.send(f"```{message}```")
+            await self.send(ctx, "warning", message)
 
         else:
-            traceback.print_exception(type(error), error, error.__traceback__)
-            await ctx.send(f"```\n{type(error).__name__}: {str(error)}```")
+            await self.log_and_traceback(ctx, error)
 
 
 def setup(bot):
-    bot.add_cog(Events(bot))
+    bot.add_cog(ErrorHander(bot))
