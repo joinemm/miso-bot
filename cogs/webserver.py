86d05d4bbcb28a6f284bd21d65205fc3d43d5122
Joinemm <joonas@rautiola.co>
2020-11-30 19:20:14 +0200

    remove unused endpoints and simplify logging

--- cogs/webserver.py
+++ cogs/webserver.py
@@ -1,7 +1,9 @@
-import asyncio
 import yaml
+from helpers import log
 from discord.ext import commands
 from aiohttp import web
+
+logger = log.get_logger(__name__)
 
 
 class WebServer(commands.Cog):
@@ -10,9 +12,8 @@ class WebServer(commands.Cog):
         self.app = web.Application()
         self.app.router.add_get("/", self.index)
         self.app.router.add_get("/guilds", self.guild_count)
+        self.app.router.add_get("/users", self.user_count)
         self.app.router.add_get("/ping", self.ping_handler)
-        self.app.router.add_get("/userinfo", self.userinfo)
-        self.app.router.add_get("/guildinfo", self.serverinfo)
         self.bot.loop.create_task(self.run())
 
     async def run(self):
@@ -20,7 +21,13 @@ class WebServer(commands.Cog):
             config = yaml.safe_load(f)
 
         self.allowed_domains = config.get("allowed_domains", "*")
-        asyncio.ensure_future(web._run_app(self.app, host=config["host"], port=config["port"]))
+        try:
+            logger.info(f"Starting webserver on {config['host']}:{config['port']}")
+            await web._run_app(
+                self.app, host=config["host"], port=config["port"], access_log=logger, print=None
+            )
+        except OSError as e:
+            logger.warning(e)
 
     async def index(self, request):
         return web.Response(text="Hi I'm Miso Bot!")
@@ -31,36 +38,8 @@ class WebServer(commands.Cog):
     async def guild_count(self, request):
         return web.Response(text=f"{len(self.bot.guilds)}")
 
-    async def serverinfo(self, request):
-        try:
-            guildid = request.rel_url.query["guildid"]
-            guild = self.bot.get_guild(int(guildid))
-            return web.json_response({
-                "name": guild.name,
-                "id": guild.id,
-                "icon": str(guild.icon_url),
-                "owner_id": guild.owner_id
-            }, headers={
-                "Access-Control-Allow-Origin": self.allowed_domains
-            })
-        except Exception as e:
-            return web.Response(text=f"Error: {e}")
-
-    async def userinfo(self, request):
-        try:
-            userid = request.rel_url.query["userid"]
-            user = self.bot.get_user(int(userid))
-            return web.json_response({
-                "name": user.name,
-                "id": user.id,
-                "discriminator": user.discriminator,
-                "avatar": str(user.avatar_url),
-                "bot": user.bot
-            }, headers={
-                "Access-Control-Allow-Origin": self.allowed_domains
-            })
-        except Exception as e:
-            return web.Response(text=f"Error: {e}")
+    async def user_count(self, request):
+        return web.Response(text=f"{len(set(self.bot.get_all_members()))}")
 
     def cog_unload(self):
         self.bot.loop.create_task(self.shutdown())
