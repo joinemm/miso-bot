03949dc4c33b7a1ac866bd6f656bd3da61753614
Joinemm <joonas@rautiola.co>
2024-03-16 18:34:46 +0200

    Upload backups to backblaze B2

--- backup.sh
+++ backup.sh
@@ -9,12 +9,10 @@ set -e
 
 BACKUP_DIR="$HOME/backups"
 CONTAINER_NAME="miso-db"
-DATABASE_NAME="misobot"
+DATABASES="misobot"
 USERLOGIN="--user=bot --password=botpw"
-DUMP_OPTIONS="--quick --add-drop-table --add-locks --extended-insert --lock-tables"
-
-# Get the current timestamp
-TS=$(date +"%F-%s")
+DUMP_OPTIONS="--force --quick --single-transaction --compact --extended-insert --order-by-primary --ignore-table="${DATABASE_NAME}.sessions""
+BUCKET="s3:s3.us-west-004.backblazeb2.com/misobot-database"
 
 # Create our backup directory if not already there
 mkdir -p "$BACKUP_DIR"
@@ -23,6 +21,17 @@ if [ ! -d "$BACKUP_DIR" ]; then
     exit 1
 fi
 
-# Dump our database
-echo "Dumping MySQL Database $DATABASE_NAME"
-docker exec "$CONTAINER_NAME" /usr/bin/mysqldump $USERLOGIN $DUMP_OPTIONS "$DATABASE_NAME" >"$BACKUP_DIR"/"$TS"-"$DATABASE_NAME".sql
+# back up the backups because why not
+cp -r "$BACKUP_DIR" "$BACKUP_DIR"-yesterday
+
+# Dump our databases
+for DATABASE_NAME in $DATABASES; do
+    echo "Dumping MySQL Database $DATABASE_NAME"
+    # shellcheck disable=SC2086
+    docker exec "$CONTAINER_NAME" /usr/bin/mysqldump $USERLOGIN $DUMP_OPTIONS "$DATABASE_NAME" >"$BACKUP_DIR"/"$DATABASE_NAME".sql
+done
+
+echo "Uploading dumps to B2"
+# shellcheck source=./.restic.env.example
+. .restic.env
+restic -r "$BUCKET" backup "$BACKUP_DIR"
